51a11aeeae1dce93c7b2a882fa658d4c33c69c6f
Nanyoung Kim <nanyoung18@gmail.com>
2021-01-03 03:58:30 +0900

    Initial commit

---
+++ src/main/java/nanyoung/nanyoungspring/NanyoungSpringApplication.java
@@ -0,0 +1,13 @@
+package nanyoung.nanyoungspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanyoungSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanyoungSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nanyoung/nanyoungspring/NanyoungSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package nanyoung.nanyoungspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanyoungSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
